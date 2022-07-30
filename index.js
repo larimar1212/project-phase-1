de42816cc7acce2ec350ec52729a8564278687a2
Nathan <namo678@me.com>
2022-07-30 19:09:25 -0400

    cleaned fetch/added score tracker

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const baseUrl = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple'
+const baseUrl = 'https://opentdb.com/api.php?amount=1&category=9&difficulty=medium&type=multiple'
 const startButton = document.getElementById('start-button')
 const dropDown = document.getElementById('drop-down')
 const dropDownLabel = document.getElementById('label')
 const playArea = document.getElementById('play-area')
-
 //creating nodes for actual quiz
 
 //where question will show up
@@ -11,38 +10,47 @@ const quizQuestion = document.createElement('h1')
 quizQuestion.id = 'question'
 quizQuestion.style.color = 'white'
 
-//answer buttons
-const answer1 = document.createElement('button')
-const answer2 = document.createElement('button')
-const answer3 = document.createElement('button')
-const answer4 = document.createElement('button')
+//keeps track of score
+let score = 0
 
+const triviaContent = (obj) => {
+    startButton.addEventListener('click', () => {
+        dropDown.remove()
+        startButton.remove()
+        dropDownLabel.remove()
+        quizQuestion.textContent = obj.question
+        playArea.append(quizQuestion)
+        let randomizedAnswers = randomize(obj) // callback function to generate randomized answers
+        randomizedAnswers.forEach((answer) => { // iterate over random answer array and append to
+            const answerButton = document.createElement('button')
+            answerButton.innerText = answer
+            playArea.append(answerButton)
+            answerButton.addEventListener('click', () => {
+                if (answer === obj['correct_answer']){
+                    score++
+                }
+                
 
-fetch(baseUrl).then(response => response.json())
-.then(data => {
+            })
+        })
     
-    //iterates though the API and then through the key where question/answers are
-    Object.keys(data).forEach(thing => {
-    data.results.forEach(q => {
-        console.log(q)
+    })
+}
 
-//when start is pressed, removes start page elements and populates
-//whith new nodes that display trivia
-startButton.addEventListener('click', () => {
-    dropDown.remove()
-    startButton.remove()
-    dropDownLabel.remove()
-    quizQuestion.textContent = q.question
-    answer1.textContent = q['correct_answer']
-    answer2.textContent = q['incorrect_answers']
-    answer3.textContent = q['incorrect_answers']
-    answer4.textContent = q['incorrect_answers']
-    playArea.append(quizQuestion, answer1, answer2, answer3, answer4)
-
-
-})
-})
+const getTrivia = () => {
+// change URL to 1 result
+fetch(baseUrl).then(req => req.json())
+.then(data => {
+    let res = data.results[0] // make a variable for the actual object
+    triviaContent(res)    
 })
 }
-)
+getTrivia()
+
+        
+const randomize = (res) => {
+    let allAnswers = [...res.incorrect_answers]
+    allAnswers.splice(Math.floor(Math.random() * (res.incorrect_answers.length + 1)), 0, res.correct_answer)
+    return allAnswers
+}
 
