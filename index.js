5827b1fc6c6623d835e32bb38bcc22356b39314d
larimar1212 <larimarfondeur@gmail.com>
2022-07-30 14:54:43 -0400

    css

--- index.js
+++ index.js
@@ -1,12 +1,133 @@
-let baseUrl = 'https://opentdb.com/api.php?amount=10'
+const _question = document.getElementById('question');
+const _options = document.querySelector('.quiz-options');
+const _checkBtn = document.getElementById('check-answer');
+const _playAgainBtn = document.getElementById('play-again');
+const _result = document.getElementById('result');
+const _correctScore = document.getElementById('correct-score');   
+const _totalQuestion = document.getElementById('total-question');
 
-const fetchData = () => {
-    fetch(baseUrl)
-    .then(req => req.json())
-    .then(res => {
-        console.log('success' )
+let correctAnswer = ""
+let correctScore = askedCount = 0
+let totalQuestion = 10;
 
-    })
+// load question from API
+async function loadQuestion() {
+    const APIUrl = 'https://opentdb.com/api.php?amount=1';
+    const result = await fetch(`${APIUrl}`)
+    const data = await result.json();
+    _result.innerHTML = "";
+    showQuestion(data.results[0]);
 }
 
-fetchData()
+// event listeners
+function eventListeners() {
+    _checkBtn.addEventListener('click', checkAnswer);
+    _playAgainBtn.addEventListener('click', restartQuiz);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadQuestion();
+    eventListeners();
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+});
+
+// show homepage as blank page (posiiton)
+// then add a select and filter + event listener to 
+// push page out of view and insert page with questions 
+
+
+
+
+
+
+function showQuestion(data) {
+    _checkBtn.disabled = false;
+    correctAnswer = data.correct_answer;
+    let incorrectAnswer = data.incorrect_answers;
+    let optionsList = incorrectAnswer;
+    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer); // randomizes the answers list
+    // console.log(correctAnswer);
+
+
+   
+    _options.innerHTML = `
+        ${optionsList.map((option, index) => `
+            <li> ${index + 1}. <span>${option}</span> </li>
+        `).join('')}
+    `;
+    selectOption();
+}
+
+
+// options selection
+function selectOption() {
+    _options.querySelectorAll('li').forEach(function (option) {
+        option.addEventListener('click', function () {
+            if (_options.querySelector('.selected')) {
+                const activeOption = _options.querySelector('.selected');
+                activeOption.classList.remove('selected');
+            }
+            option.classList.add('selected');
+        });
+    });
+}
+
+// answer checking
+function checkAnswer() {
+    _checkBtn.disabled = true;
+    if (_options.querySelector('.selected')) {
+        let selectedAnswer = _options.querySelector('.selected span').textContent;
+        if (selectedAnswer == HTMLDecode(correctAnswer)) {
+            correctScore++;
+            _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
+        } else {
+            _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
+        }
+        checkCount();
+    } else {
+        _result.innerHTML = `<p><i class = "fas fa-question"></i>Please select an option!</p>`;
+        _checkBtn.disabled = false;
+    }
+}
+
+// to convert html entities into normal text of correct answer if there is any
+function HTMLDecode(textString) {
+    let doc = new DOMParser().parseFromString(textString, "text/html");
+    return doc.documentElement.textContent;
+}
+
+
+function checkCount() {
+    askedCount++;
+    setCount();
+    if (askedCount == totalQuestion) {
+        setTimeout(function () {
+            console.log("");
+        }, 5000);
+
+
+        _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
+        _playAgainBtn.style.display = "block";
+        _checkBtn.style.display = "none";
+    } else {
+        setTimeout(function () {
+            loadQuestion();
+        }, 300);
+    }
+}
+
+function setCount() {
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+}
+
+
+function restartQuiz() {
+    correctScore = askedCount = 0;
+    _playAgainBtn.style.display = "none";
+    _checkBtn.style.display = "block";
+    _checkBtn.disabled = false;
+    setCount();
+    loadQuestion();
+}
