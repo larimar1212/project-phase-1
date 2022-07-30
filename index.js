c57b27c914b2ea7274008c8d9a25fd25238a5a11
larimar1212 <larimarfondeur@gmail.com>
2022-07-30 17:58:10 -0400

    random answers appear on start

--- index.js
+++ index.js
@@ -1,133 +1,39 @@
-const _question = document.getElementById('question');
-const _options = document.querySelector('.quiz-options');
-const _checkBtn = document.getElementById('check-answer');
-const _playAgainBtn = document.getElementById('play-again');
-const _result = document.getElementById('result');
-const _correctScore = document.getElementById('correct-score');   
-const _totalQuestion = document.getElementById('total-question');
+const baseUrl = 'https://opentdb.com/api.php?amount=1&category=9&difficulty=medium&type=multiple'
+const startButton = document.getElementById('start-button')
+const dropDown = document.getElementById('drop-down')
+const dropDownLabel = document.getElementById('label')
+const playArea = document.getElementById('play-area')
 
-let correctAnswer = ""
-let correctScore = askedCount = 0
-let totalQuestion = 10;
+//creating nodes for actual quiz
 
-// load question from API
-async function loadQuestion() {
-    const APIUrl = 'https://opentdb.com/api.php?amount=1';
-    const result = await fetch(`${APIUrl}`)
-    const data = await result.json();
-    _result.innerHTML = "";
-    showQuestion(data.results[0]);
+//where question will show up
+const quizQuestion = document.createElement('h1')
+quizQuestion.id = 'question'
+quizQuestion.style.color = 'white'
+
+// change URL to 1 result
+fetch(baseUrl).then(req => req.json())
+.then(data => {
+    let res = data.results[0] // make a variable for the actual object
+    startButton.addEventListener('click', () => {
+        dropDown.remove()
+        startButton.remove()
+        dropDownLabel.remove()
+        quizQuestion.textContent = res.question
+        playArea.append(quizQuestion)
+        let randomizedAnswers = randomize(res) // callback function to generate randomized answers
+        randomizedAnswers.forEach((answer) => { // iterate over random answer array and append to
+            const answerButton = document.createElement('button')
+            answerButton.innerText = answer
+            playArea.append(answerButton)
+        })
+        
+    })
+})
+        
+const randomize = (res) => {
+    let allAnswers = [...res.incorrect_answers]
+    allAnswers.splice(Math.floor(Math.random() * (res.incorrect_answers.length + 1)), 0, res.correct_answer)
+    return allAnswers
 }
 
-// event listeners
-function eventListeners() {
-    _checkBtn.addEventListener('click', checkAnswer);
-    _playAgainBtn.addEventListener('click', restartQuiz);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    loadQuestion();
-    eventListeners();
-    _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
-});
-
-// show homepage as blank page (posiiton)
-// then add a select and filter + event listener to 
-// push page out of view and insert page with questions 
-
-
-
-
-
-
-function showQuestion(data) {
-    _checkBtn.disabled = false;
-    correctAnswer = data.correct_answer;
-    let incorrectAnswer = data.incorrect_answers;
-    let optionsList = incorrectAnswer;
-    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer); // randomizes the answers list
-    // console.log(correctAnswer);
-
-
-   
-    _options.innerHTML = `
-        ${optionsList.map((option, index) => `
-            <li> ${index + 1}. <span>${option}</span> </li>
-        `).join('')}
-    `;
-    selectOption();
-}
-
-
-// options selection
-function selectOption() {
-    _options.querySelectorAll('li').forEach(function (option) {
-        option.addEventListener('click', function () {
-            if (_options.querySelector('.selected')) {
-                const activeOption = _options.querySelector('.selected');
-                activeOption.classList.remove('selected');
-            }
-            option.classList.add('selected');
-        });
-    });
-}
-
-// answer checking
-function checkAnswer() {
-    _checkBtn.disabled = true;
-    if (_options.querySelector('.selected')) {
-        let selectedAnswer = _options.querySelector('.selected span').textContent;
-        if (selectedAnswer == HTMLDecode(correctAnswer)) {
-            correctScore++;
-            _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
-        } else {
-            _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
-        }
-        checkCount();
-    } else {
-        _result.innerHTML = `<p><i class = "fas fa-question"></i>Please select an option!</p>`;
-        _checkBtn.disabled = false;
-    }
-}
-
-// to convert html entities into normal text of correct answer if there is any
-function HTMLDecode(textString) {
-    let doc = new DOMParser().parseFromString(textString, "text/html");
-    return doc.documentElement.textContent;
-}
-
-
-function checkCount() {
-    askedCount++;
-    setCount();
-    if (askedCount == totalQuestion) {
-        setTimeout(function () {
-            console.log("");
-        }, 5000);
-
-
-        _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
-        _playAgainBtn.style.display = "block";
-        _checkBtn.style.display = "none";
-    } else {
-        setTimeout(function () {
-            loadQuestion();
-        }, 300);
-    }
-}
-
-function setCount() {
-    _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
-}
-
-
-function restartQuiz() {
-    correctScore = askedCount = 0;
-    _playAgainBtn.style.display = "none";
-    _checkBtn.style.display = "block";
-    _checkBtn.disabled = false;
-    setCount();
-    loadQuestion();
-}
