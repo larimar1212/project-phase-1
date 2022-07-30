484b5aa6a53a07b7c8ecc54de2ecef74099c40c8
Nathan <namo678@me.com>
2022-07-30 17:02:02 -0400

    trying stuff

--- index.js
+++ index.js
@@ -1,133 +1,48 @@
-const _question = document.getElementById('question');
-const _options = document.querySelector('.quiz-options');
-const _checkBtn = document.getElementById('check-answer');
-const _playAgainBtn = document.getElementById('play-again');
-const _result = document.getElementById('result');
-const _correctScore = document.getElementById('correct-score');   
-const _totalQuestion = document.getElementById('total-question');
+const baseUrl = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple'
+const startButton = document.getElementById('start-button')
+const dropDown = document.getElementById('drop-down')
+const dropDownLabel = document.getElementById('label')
+const playArea = document.getElementById('play-area')
 
-let correctAnswer = ""
-let correctScore = askedCount = 0
-let totalQuestion = 10;
+//creating nodes for actual quiz
 
-// load question from API
-async function loadQuestion() {
-    const APIUrl = 'https://opentdb.com/api.php?amount=1';
-    const result = await fetch(`${APIUrl}`)
-    const data = await result.json();
-    _result.innerHTML = "";
-    showQuestion(data.results[0]);
+//where question will show up
+const quizQuestion = document.createElement('h1')
+quizQuestion.id = 'question'
+quizQuestion.style.color = 'white'
+
+//answer buttons
+const answer1 = document.createElement('button')
+const answer2 = document.createElement('button')
+const answer3 = document.createElement('button')
+const answer4 = document.createElement('button')
+
+
+fetch(baseUrl).then(response => response.json())
+.then(data => {
+    
+    //iterates though the API and then through the key where question/answers are
+    Object.keys(data).forEach(thing => {
+    data.results.forEach(q => {
+        console.log(q)
+
+//when start is pressed, removes start page elements and populates
+//whith new nodes that display trivia
+startButton.addEventListener('click', () => {
+    dropDown.remove()
+    startButton.remove()
+    dropDownLabel.remove()
+    quizQuestion.textContent = q.question
+    answer1.textContent = q['correct_answer']
+    answer2.textContent = q['incorrect_answers']
+    answer3.textContent = q['incorrect_answers']
+    answer4.textContent = q['incorrect_answers']
+    playArea.append(quizQuestion, answer1, answer2, answer3, answer4)
+
+
+})
+})
+})
 }
+)
 
-// event listeners
-function eventListeners() {
-    _checkBtn.addEventListener('click', checkAnswer);
-    _playAgainBtn.addEventListener('click', restartQuiz);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    loadQuestion();
-    eventListeners();
-    _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
-});
-
-// show homepage as blank page (posiiton)
-// then add a select and filter + event listener to 
-// push page out of view and insert page with questions 
-
-
-
-
-
-
-function showQuestion(data) {
-    _checkBtn.disabled = false;
-    correctAnswer = data.correct_answer;
-    let incorrectAnswer = data.incorrect_answers;
-    let optionsList = incorrectAnswer;
-    optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer); // randomizes the answers list
-    // console.log(correctAnswer);
-
-
-   
-    _options.innerHTML = `
-        ${optionsList.map((option, index) => `
-            <li> ${index + 1}. <span>${option}</span> </li>
-        `).join('')}
-    `;
-    selectOption();
-}
-
-
-// options selection
-function selectOption() {
-    _options.querySelectorAll('li').forEach(function (option) {
-        option.addEventListener('click', function () {
-            if (_options.querySelector('.selected')) {
-                const activeOption = _options.querySelector('.selected');
-                activeOption.classList.remove('selected');
-            }
-            option.classList.add('selected');
-        });
-    });
-}
-
-// answer checking
-function checkAnswer() {
-    _checkBtn.disabled = true;
-    if (_options.querySelector('.selected')) {
-        let selectedAnswer = _options.querySelector('.selected span').textContent;
-        if (selectedAnswer == HTMLDecode(correctAnswer)) {
-            correctScore++;
-            _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
-        } else {
-            _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
-        }
-        checkCount();
-    } else {
-        _result.innerHTML = `<p><i class = "fas fa-question"></i>Please select an option!</p>`;
-        _checkBtn.disabled = false;
-    }
-}
-
-// to convert html entities into normal text of correct answer if there is any
-function HTMLDecode(textString) {
-    let doc = new DOMParser().parseFromString(textString, "text/html");
-    return doc.documentElement.textContent;
-}
-
-
-function checkCount() {
-    askedCount++;
-    setCount();
-    if (askedCount == totalQuestion) {
-        setTimeout(function () {
-            console.log("");
-        }, 5000);
-
-
-        _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
-        _playAgainBtn.style.display = "block";
-        _checkBtn.style.display = "none";
-    } else {
-        setTimeout(function () {
-            loadQuestion();
-        }, 300);
-    }
-}
-
-function setCount() {
-    _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
-}
-
-
-function restartQuiz() {
-    correctScore = askedCount = 0;
-    _playAgainBtn.style.display = "none";
-    _checkBtn.style.display = "block";
-    _checkBtn.disabled = false;
-    setCount();
-    loadQuestion();
-}
