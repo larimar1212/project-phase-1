202023a0357a04f0fe5bf88e74aac8f14a929aa7
larimar1212 <larimarfondeur@gmail.com>
2022-07-31 15:26:36 -0400

    changed js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const startButton = document.getElementById('start-button')
 const dropDown = document.getElementById('drop-down')
 const dropDownLabel = document.getElementById('label')
 const playArea = document.getElementById('play-area')
-
 //creating nodes for actual quiz
 
 //where question will show up
@@ -11,29 +10,68 @@ const quizQuestion = document.createElement('h1')
 quizQuestion.id = 'question'
 quizQuestion.style.color = 'white'
 
-// change URL to 1 result
-fetch(baseUrl).then(req => req.json())
-.then(data => {
-    let res = data.results[0] // make a variable for the actual object
-    startButton.addEventListener('click', () => {
-        dropDown.remove()
-        startButton.remove()
-        dropDownLabel.remove()
-        quizQuestion.textContent = res.question
-        playArea.append(quizQuestion)
-        let randomizedAnswers = randomize(res) // callback function to generate randomized answers
-        randomizedAnswers.forEach((answer) => { // iterate over random answer array and append to
-            const answerButton = document.createElement('button')
-            answerButton.innerText = answer
-            playArea.append(answerButton)
+//keeps track of score
+let score = 0
+
+// 2.
+const triviaContent = (questionObject) => {
+    quizQuestion.innerHTML = questionObject.question
+    playArea.append(quizQuestion)
+
+    // .3
+    let randomizedAnswers = randomize(questionObject) // callback function to generate randomized answers
+    randomizedAnswers.forEach((answer) => { // iterate over random answer array and append to
+        const answerButton = document.createElement('button')
+        answerButton.innerHTML = answer
+        playArea.append(answerButton)
+        answerButton.addEventListener('click', () => {
+            if (answer === questionObject['correct_answer']) {
+                score++
+            }
+            console.log(score)
+            playArea.innerHTML = ''
+            getTrivia()
+
+
         })
-        
     })
-})
-        
-const randomize = (res) => {
-    let allAnswers = [...res.incorrect_answers]
-    allAnswers.splice(Math.floor(Math.random() * (res.incorrect_answers.length + 1)), 0, res.correct_answer)
+}
+
+
+
+// 1.
+const getTrivia = () => {
+    // change URL to 1 result
+    fetch(baseUrl).then(req => req.json())
+        .then(data => {
+            let questionObject = data.results[0] // make a variable for the actual object
+            triviaContent(questionObject)
+        })
+
+}
+
+// 4.
+const randomize = (questionObject) => {
+    let allAnswers = [...questionObject.incorrect_answers]
+    allAnswers.splice(Math.floor(Math.random() * (questionObject.incorrect_answers.length + 1)), 0, questionObject.correct_answer)
     return allAnswers
 }
 
+
+// funciton when u click start, and when click on answer 
+
+// clicking start and invoking the removal of the elements 
+startButton.addEventListener('click', () => {
+    handleClickStart()
+
+})
+
+
+// removing these elements from the page after clicking start
+const handleClickStart = () => {
+    dropDown.remove()
+    startButton.remove()
+    dropDownLabel.remove()
+    getTrivia() // invoking original fetch req to render next question
+
+}
